d1216d181934bcc025a9f8fce302168180eb2c0f
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2021-06-20 13:10:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,15 +7,15 @@ if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
 	newaliases
 fi
 if [ ! -z "$LANGUAGE" ]; then
-	sed -i "s/^language=en$/language=$LANGUAGE/g" /tmp/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^language=en$/language=$LANGUAGE/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
 if [ ! -z "$COUNTRY" ]; then
-	sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /tmp/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
 if [ ! -z "$HOSTNAME" ]; then
-	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
-	sed -i "s/^ssl_cert_common_name=server1.example.com$/ssl_cert_common_name=$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
-	sed -i "s/^ssl_cert_email=hostmaster@example.com$/ssl_cert_email=hostmaster@$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^ssl_cert_common_name=server1.example.com$/ssl_cert_common_name=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^ssl_cert_email=hostmaster@example.com$/ssl_cert_email=hostmaster@$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
 if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
 	mysql_install_db
