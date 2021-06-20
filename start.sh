5dc33a628564e0f353b55f876027b758f45ad4f2
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2021-06-20 14:37:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,41 +6,50 @@ if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
 	newlist -q mailman $(MAILMAN_EMAIL) $(MAILMAN_PASS)
 	newaliases
 fi
-if [ ! -z "$LANGUAGE" ]; then
-	sed -i "s/^language=en$/language=$LANGUAGE/g" /root/ispconfig3_install/install/autoinstall.ini
+if [ ! -z "$ISPC_LANGUAGE" ]; then
+	sed -i "s/^language=en$/language=$ISPC_LANGUAGE/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
-if [ ! -z "$COUNTRY" ]; then
+if [ ! -z "$ISPC_COUNTRY" ]; then
 	sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
-if [ ! -z "$HOSTNAME" ]; then
-	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
-	sed -i "s/^ssl_cert_common_name=server1.example.com$/ssl_cert_common_name=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
-	sed -i "s/^ssl_cert_email=hostmaster@example.com$/ssl_cert_email=hostmaster@$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+if [ ! -z "$ISPC_HOSTNAME" ]; then
+	sed -i "s/^hostname=server1.example.com$/hostname=$ISPC_HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^ssl_cert_common_name=server1.example.com$/ssl_cert_common_name=$ISPC_HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+	sed -i "s/^ssl_cert_email=hostmaster@example.com$/ssl_cert_email=hostmaster@$ISPC_HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
 fi
+if [ ! -z "$ISPC_MYSQL_HOST" ]; then
+	sed -i "s/^mysql_hostname=localhost$/mysql_hostname=$ISPC_MYSQL_HOST/g" /root/ispconfig3_install/install/autoinstall.ini
+fi
+if [ ! -z "$ISPC_MYSQL_PASS" ]; then
+	sed -i "s/^mysql_root_password=pass$/mysql_root_password=$ISPC_MYSQL_PASS/g" /root/ispconfig3_install/install/autoinstall.ini
+fi
+
+
 if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
-	mysql_install_db
-	service mysql start \
-	&& echo "UPDATE mysql.user SET Password = PASSWORD('pass') WHERE User = 'root';" | mysql -u root \
-	&& echo "UPDATE mysql.user SET plugin='mysql_native_password' where user='root';" | mysql -u root \
-	&& echo "DELETE FROM mysql.user WHERE User='';" | mysql -u root \
-	&& echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -u root \
-	&& echo "DROP DATABASE IF EXISTS test;" | mysql -u root \
-	&& echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" | mysql -u root \
-	&& echo "FLUSH PRIVILEGES;" | mysql -u root
-	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+#	mysql_install_db
+#	service mysql start \
+#	&& echo "UPDATE mysql.user SET Password = PASSWORD('pass') WHERE User = 'root';" | mysql -u root \
+#	&& echo "UPDATE mysql.user SET plugin='mysql_native_password' where user='root';" | mysql -u root \
+#	&& echo "DELETE FROM mysql.user WHERE User='';" | mysql -u root \
+#	&& echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -u root \
+#	&& echo "DROP DATABASE IF EXISTS test;" | mysql -u root \
+#	&& echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" | mysql -u root \
+#	&& echo "FLUSH PRIVILEGES;" | mysql -u root
 	# RUN mysqladmin -u root password pass
-	service mysql start && php -q /root/ispconfig3_install/install/install.php --autoinstall=/root/ispconfig3_install/install/autoinstall.ini
+	php -q /root/ispconfig3_install/install/install.php --autoinstall=/root/ispconfig3_install/install/autoinstall.ini
 	mkdir -p /var/www/html
 	echo "" > /var/www/html/index.html
 	rm -r /root/ispconfig3_install
+else
+	php -q /root/ispconfig3_install/install/update.php --autoinstall=/root/ispconfig3_install/install/autoinstall.ini
 fi
+
+if [ ! -z "$ISPC_PASSWORD" ]; then
+	echo "USE dbispconfig;UPDATE sys_user SET passwort = md5('$ISPC_PASSWORD') WHERE username = 'admin';" | mysql -h $ISPC_MYSQL_HOST -u root -p$ISPC_MYSQL_PASS
+fi
+
 
 #service mysql start && php -q /tmp/ispconfig3_install/install/install.php --autoinstall=/tmp/ispconfig3_install/install/autoinstall.ini
-
-if [ ! -z "$PASSWORD" ]; then
-	service mysql start \
-	&& echo "USE dbispconfig;UPDATE sys_user SET passwort = md5('$PASSWORD') WHERE username = 'admin';" | mysql -u root -ppass
-fi
 
 screenfetch
 
